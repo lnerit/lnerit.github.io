d0f28be7f05f2a0fbadf475c5c01bb53915bfe40
mykldog7 <mykldog@gmail.com>
2015-06-25 14:29:49 +1200

    Mike colors.

--- ANZChampionship/visual2.js
+++ ANZChampionship/visual2.js
@@ -5,6 +5,9 @@
  */
 
 "use strict";
+var colorScale;
+var greatestPointsDifference=50;
+
 function makeVisualizationTwo(theData, svg){
 	//Sanity Check.... got data.. got d3?
 	if(theData == undefined || svg == undefined){
@@ -28,6 +31,7 @@ function makeVisualizationTwo(theData, svg){
 	var indexes = {home : {win: 0, loss:0},
 		       away : {win: 0, loss:0}
 		      }
+	 greatestPointsDifference=0;
 
 	//make each game object have a isHomeGame and pointsDifference field.
 	gameSet.forEach(function(game, i){
@@ -39,6 +43,9 @@ function makeVisualizationTwo(theData, svg){
 			game.isHomeGame = false;
 			game.pointsDifference = game.Score.split("–")[1] - game.Score.split("–")[0];
 		}
+		//store greatest point Difference to give color scaler.
+		if(greatestPointsDifference < Math.abs(game.pointsDifference)){ greatestPointsDifference = Math.abs(game.pointsDifference);}
+		
 		//store the index so that we can build FDLayout later. 
 		if(game.isHomeGame){
 			if(game.pointsDifference > 0){indexes.home.win = i;}
@@ -72,19 +79,31 @@ function makeVisualizationTwo(theData, svg){
 
 	//force directed layout //link: https://github.com/mbostock/d3/wiki/Force-Layout
 	var force = d3.layout.force()
-		.charge(-119)
+		.charge(-129)
 		.size([visualizationWidth, visualizationHeight])
 		.links(links)
-		.linkDistance(0.7)
+		.linkDistance(0.8)
 		.nodes(gameSet)
 		.gravity(0.14)
 		.start();
 
+	//setup color scaler
+	colorScale = d3.scale.linear()
+		    .domain([-greatestPointsDifference, 0, greatestPointsDifference])
+		    .interpolate(d3.interpolateRgb)
+		    .range([d3.rgb(255,0,0),"grey", d3.rgb(0,255,0)]);
+		
 	//set of circles ...
 	var circles = svgElem.selectAll("circle")
 		.data(gameSet)
 		.enter()
-		.append("circle");
+		.append("circle")
+		.attr("r",10)
+		.attr("fill", function(game,i){
+					var pD = game.pointsDifference;
+					return (colorScale(pD));
+		})
+		.on('mouseover', function(d) {mouseOverCircle(d);});
 
 	//set of labels.
 	var labels = svgElem.selectAll("text")
@@ -98,25 +117,26 @@ function makeVisualizationTwo(theData, svg){
 	force.on("tick", function() {
 		//random x and y for the start of the force directed layout.
 		circles.attr("cx", function(d) {return d.x})
-			.attr("cy", function(d) {return d.y})
-			.attr("r",8)
-			.attr("fill", function(game,i){
-					return ("rgb("+(140 - (11*game.pointsDifference))+","+(140 +(11*game.pointsDifference))+","+30+")");
-			});
-
+			.attr("cy", function(d) {return d.y});
+	
 		//update labels depending on their content. as the focui of the two clusters move.
 		labels.attr("x", function(data){
 			//Half widths for(to center labels )halfWinnerLabelWidth = 42 halfLoserLabelWidth = 31;
-			if(data.lbl=="Home!"){ return gameSet[indexes.home.win].x-42; }
-			else { return gameSet[indexes.away.win].x-31; }
+			if(data.lbl=="Home!"){ return gameSet[indexes.home.win].x-22; }
+			else { return gameSet[indexes.away.win].x-15; }
 		})
 		.attr("y", function(data){
-			if(data.lbl=="Home!"){ return gameSet[indexes.home.win].y-68; }
-			else { return gameSet[indexes.away.win].y-68; }
+			if(data.lbl=="Home!"){ return gameSet[indexes.home.win].y-75; }
+			else { return gameSet[indexes.away.win].y-75; }
 		});
-
 	});
+} //end makeVisualizationTwo
 
+/* On mouseover for each circle. */
+function mouseOverCircle(gameRepresentedByCircle){
+   console.log("You mousedOver my game" + JSON.stringify(gameRepresentedByCircle)); 
 }
+
+
 
 
